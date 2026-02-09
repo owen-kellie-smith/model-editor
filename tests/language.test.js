34be1e4602bb240653831c858858a9753bb35b32
Owen Kellie-Smith <oks.modelling@gmail.com>
2026-02-09 06:29:17 +0000

    Language tests for function with no name or NAN arity

--- tests/language.test.js
+++ tests/language.test.js
@@ -9,12 +9,32 @@ import { getFunctionsFromLanguage } from "../docs/src/domain/language.js";
 
 
 describe("language loading", () => {
-  it("reads functions from XML", () => {
-    const fixture = getFixture("language.xml");
-    const xml = loadXml(fixture); // adjust if path differs
-    const lang = getFunctionsFromLanguage(xml, "test");
-
-    expect(lang.functions.size).toBeGreaterThan(0);
+  describe("when XML is valid", () => {
+    describe("when XML has functions with names and arity", () => {
+      it("reads functions from XML", () => {
+        const fixture = getFixture("language.xml");
+        const xml = loadXml(fixture); 
+        const lang = getFunctionsFromLanguage(xml, "test");
+        expect(lang.functions.size)
+          .toBeGreaterThan(0);
+      });
+    });
+    describe("when XML has a function with no name", () => {
+      it("throws an error", () => {
+        const fixture = getFixture("languageNoName.xml");
+        const xml = loadXml(fixture); 
+        expect(() => getFunctionsFromLanguage(xml, "test"))
+          .toThrow(/function without name/i);
+      });
+    });
+    describe("when XML has a function with non-numeric arity", () => {
+      it("throws an error", () => {
+        const fixture = getFixture("languageNaNArity.xml");
+        const xml = loadXml(fixture); 
+        expect(() => getFunctionsFromLanguage(xml, "test"))
+          .toThrow(/Invalid function/i);
+      });
+    });
   });
 });
 
