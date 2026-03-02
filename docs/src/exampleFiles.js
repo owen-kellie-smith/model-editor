0aedf2faed209c932203a44be6e6ebe5d3eab58c
Owen Kellie-Smith <oks.modelling@gmail.com>
2026-03-02 07:16:21 +0000

    Larger font size for ?large paameter

--- docs/src/exampleFiles.js
+++ docs/src/exampleFiles.js
@@ -1,9 +1,11 @@
 export const exampleFiles = [
   { category: "language", path: "docs/examples/language.xml" },
-  { category: "model", path: "docs/examples/restaurant-model/restaurantNoIndices.xml" },
-  { category: "model", path: "docs/examples/restaurant-model/model.xml" },
   { category: "model", path: "docs/examples/annuity-model/vendor-format-model.xml" },
+  { category: "model", path: "docs/examples/rocket-model/rocket_with_thrust.xml" },
+  { category: "model", path: "docs/examples/rocket-model/lorenz-difference.xml" },
+  { category: "model", path: "docs/examples/restaurant-model/restaurantNoIndices.xml" },
+  { category: "model", path: "docs/examples/restaurant-model/seasonal.xml" },
+  { category: "model", path: "docs/examples/restaurant-model/model.xml" },
   { category: "model", path: "docs/examples/airline-model/model.xml" },
   { category: "model", path: "docs/examples/airline-model/Dividends.xml" },
-  { category: "model", path: "docs/examples/restaurant-model/seasonal.xml" },
 ];
